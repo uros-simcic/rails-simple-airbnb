fea0c38f03c620bcf1d0d82b7646d6f0408ac9a7
uros simcic <inbox.si@gmail.com>
2020-08-29 17:48:07 -0300

    implemented new and create actions for flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,17 +1,23 @@
 class FlatsController < ApplicationController
   def index
+    @flats = Flat.all
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    render :show
   end
 
   def edit
   end
 
   def show
+
   end
 
   def update
@@ -19,4 +25,9 @@ class FlatsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def flat_params
+      params.require(:flat).permit(:name, :address, :description, :price_per_night)
+    end
 end
